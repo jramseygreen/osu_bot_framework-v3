cfbc5021433628a650d084b9a5bb9a7f7f936331
jramseygreen <44009946+jramseygreen@users.noreply.github.com>
2021-12-10 21:25:32 +0000

    high rollers profile now functional

--- logic_profiles/high_rollers.py
+++ logic_profiles/high_rollers.py
@@ -6,6 +6,8 @@ class HighRollers:
         self.bot = bot
         self.channel = channel
         self.rolls = {}
+        self.validated = []
+        channel.set_command("!roll", self.roll)
 
     def on_match_finish(self):
         self.rolls = {}
@@ -20,9 +22,22 @@ class HighRollers:
         elif self.channel.has_users():
             self.channel.set_host(self.channel.get_formatted_users()[0])
 
+    def on_match_abort(self):
+        self.on_match_finish()
+
+    def on_join(self, username):
+        if self.channel.get_users() == [username]:
+            self.channel.set_host(username)
+
     def on_message(self, message):
-        if message["username"] == "BanchoBot":
+        if message["username"] == "BanchoBot" and " rolls " in message["content"]:
             user = message["content"][:message["content"].find("rolls")].strip()
-            if user not in self.rolls:
+            if user not in self.rolls and user in self.validated:
                 points = int(message["content"].replace(user + " rolls ", "").split(" ", 1)[0])
                 self.rolls[user] = points
+
+    def roll(self, message):
+        if message["content"] == "!roll":
+            self.validated.append(message["username"])
+        else:
+            self.channel.send_message(message["username"] + " you may only type '!roll'")
